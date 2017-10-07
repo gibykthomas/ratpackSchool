84d31c6b4f5adf57d69114137ae689eb81cd399e
jarekratajski <jratajski@gmail.com>
2017-10-07 21:53:43 +0200

    increment added

--- src/main/java/pl/setblack/javafun/ratschool/MyServer.java
+++ src/main/java/pl/setblack/javafun/ratschool/MyServer.java
@@ -2,19 +2,28 @@ package pl.setblack.javafun.ratschool;
 
 import ratpack.server.RatpackServer;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class MyServer {
+
+    private static final AtomicLong cnt = new AtomicLong(0);
+
     public static void main(String[] args) throws Exception {
         RatpackServer.start(server ->
                 server
                         .serverConfig(cfg->cfg.development(true))
                 .handlers(chain -> chain
                         .prefix("add", add ->add
-                        .get(":n",
+                            .get(":n",
                                 ctx -> {
                                         final long n = Long.parseLong(ctx.getPathTokens().get("n"));
                                         ctx.render(String.valueOf(n+1));
                                 })
-                        )
+                            )
+                        .prefix("counter", counter ->
+                                counter.post("inc", ctx ->
+                                    ctx.render(String.valueOf(cnt.addAndGet(1)))
+                                ))
                 )
         );
     }
